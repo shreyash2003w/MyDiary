ec916d566f7ad019a2b597b3129df10bb045c299
unknown <shreyashwaghmare2019@gmail.com>
2023-07-30 13:53:16 +0530

    Port update

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const contactContent =
   "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 const postseckey = process.env.SECPOSTKEY;
 const app = express();
-
+const PORT = 3000 || process.env.PORT;
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -212,6 +212,6 @@ app.get("/logout", function (req, res) {
   });
 });
 
-app.listen(3000 || process.env.PORT   , function () {
+app.listen(PORT , function () {
   console.log("Server started on port 3000");
 });
